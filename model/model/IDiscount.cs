8f87219ae34a062af0fe070fa2fdd7f0e4b1467e
RoGoZiLiO <rogozin.r998@gmail.com>
2018-05-27 18:08:44 +0700

    + комментарии в IDiscount

--- model/model/IDiscount.cs
+++ model/model/IDiscount.cs
@@ -12,12 +12,24 @@ namespace model
         /// </summary>
         double Discount(Product product);
 
+        /// <summary>
+        /// Цена товара
+        /// </summary>
         double Price { get; }
 
+        /// <summary>
+        /// Процентная скидка
+        /// </summary>
         double Percent { get; }
 
+        /// <summary>
+        /// Скидка по сертификату
+        /// </summary>
         double Certificate { get; }
 
+        /// <summary>
+        /// Цена товара с учетом скидки
+        /// </summary>
         double Result { get; }
     }
 }
