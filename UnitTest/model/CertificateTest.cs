0ded2a8a08796dd8aa245934cc919110afe199a0
RoGoZiLiO <rogozin.r998@gmail.com>
2018-05-29 19:51:31 +0700

    + unit test

---
+++ UnitTest/model/CertificateTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using model;
+using System;
+
+namespace UnitTest.model
+{
+    [TestFixture]
+    public class CertificateUnitTest
+    {
+        [Test]
+        [TestCase(0, TestName = "Тестирование Size при присваивании 0")]
+        [TestCase(1, TestName = "Тестирование Size при присваивании 1")]
+        [TestCase(99999, TestName = "Тестирование Size при присваивании 99999")]
+        [TestCase(-1, TestName = "Тестирование Size при присваивании -1")]
+        [TestCase(1.5, TestName = "Тестирование Size при присваивании 1,5")]
+        public void CertificateTest(double value)
+        {
+            CertificateDiscount certificate = new CertificateDiscount();
+            certificate.Size = value;
+        }
+
+        [Test]
+        [TestCase(TestName = "Тестирование подсчета скидки по сертификату")]
+        public void DiscountCertificateTest()
+        {
+            Product product = new Product();
+            product.Price = 200;
+            CertificateDiscount certificate = new CertificateDiscount();
+            certificate.Size = 10;
+            IDiscount discount = certificate;
+
+            Assert.AreEqual(discount.Discount(product), 190);
+        }
+    }
+}
---
+++ UnitTest/model/PercentTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using model;
+
+namespace UnitTest.model
+{
+    [TestFixture]
+    class PercentUnitTest
+    {
+        [Test]
+        [TestCase(0, TestName = "Тестирование Size при присваивании 0")]
+        [TestCase(1, TestName = "Тестирование Size при присваивании 1")]
+        [TestCase(90, TestName = "Тестирование Size при присваивании 90")]
+        [TestCase(91, TestName = "Тестирование Size при присваивании 91")]
+        [TestCase(1.5, TestName = "Тестирование Size при присваивании 1,5")]
+        public void PercentTest(double value)
+        {
+            PercentDiscount percent = new PercentDiscount();
+            percent.Cost = value;
+        }
+
+        [Test]
+        [TestCase(TestName = "Тестирование подсчета скидки по процентам")]
+        public void DiscountPercenttest()
+        {
+            Product product = new Product();
+            product.Price = 200;
+            PercentDiscount certificate = new PercentDiscount();
+            certificate.Cost = 10;
+            IDiscount discount = certificate;
+
+            Assert.AreEqual(discount.Discount(product), 180);
+        }
+    }
+}
